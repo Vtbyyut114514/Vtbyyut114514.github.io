922ebf5b3be5a61c70e3e3b58c5bd9a8fe90e3ff
Vtbyyut114514 <108570485+Vtbyyut114514@users.noreply.github.com>
2022-07-02 20:39:00 +0800

    Add files via upload

--- static/index.js
+++ static/index.js
@@ -343,11 +343,11 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 30) return '鸡你太美';
-    if (score <= 60) return '鸡你太太美';
-    if (score <= 100) return '鸡鸡鸡鸡鸡';
-    if (score <= 150) return '唱 跳 rap 篮球';
-    return '你就是蔡徐坤本人？';
+    if (score <= 30) return 'na你太美';
+    if (score <= 60) return 'na你太太美';
+    if (score <= 100) return 'nagi拉屎中';
+    if (score <= 150) return '支na猪';
+    return '你就是nagi本人？';
 }
 
 function toStr(obj) {
